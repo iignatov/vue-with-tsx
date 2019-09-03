14a2cd42cbfd9bef03e901bb8b4878a105af1a41
Ignat Ignatov <ignat.ignatov@leanplum.com>
2019-09-04 01:45:53 +0300

    Upgraded the TSX setup and all dependencies.

--- src/@types/tsx.d.ts
+++ src/@types/tsx.d.ts
@@ -1,14 +1,21 @@
-import Vue, { VNode } from 'vue';
+import Vue, { VNode } from "vue";
 
 declare global {
   namespace JSX {
-    // tslint:disable no-empty-interface
-    interface Element extends VNode {}
-    // tslint:disable no-empty-interface
-    interface ElementClass extends Vue {}
+    // tslint:disable-next-line:no-empty-interface
+    interface Element extends VNode { }
+
+    interface ElementAttributesProperty {
+      $props?: any;
+    }
+
+    // tslint:disable-next-line:no-empty-interface
+    interface ElementClass extends Vue { }
+
     interface IntrinsicElements {
       [elem: string]: any;
     }
+
     // Allows to use HTML attributes on custom components,
     // see: https://stackoverflow.com/a/53017665
     // tslint:disable-next-line:interface-over-type-literal
--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,30 @@
+const path = require("path");
+
 module.exports = {
-    chainWebpack: config => {
-        config.module.rule('tsx').test(/\.tsx$/)
-            .use('babel-loader')
-            .loader('babel-loader')
-            .options({
-                presets: ['@babel/preset-env'],
-                plugins: ['transform-vue-jsx']
-            })
-            .before('ts-loader');
+  chainWebpack: config => {
+    config.module.rule("tsx").test(/\.tsx$/)
+      .use("babel-loader")
+      .loader("babel-loader")
+      .options({
+        presets: [
+          ["@vue/babel-preset-jsx", {
+            functional: true,
+            injectH: true,
+            vModel: true,
+            vOn: false,
+          }]
+        ]
+      })
+      .before("ts-loader");
+
+    // NOTE: Keep in sync with the paths in `tsconfig.json`.
+    config.resolve.alias.set("@", path.resolve("./src"));
+  },
+  devServer: {
+    // http://expressjs.com/en/4x/api.html#express.static
+    staticOptions: {
+      fallthrough: false,
+      redirect: false,
     },
+  },
 };
